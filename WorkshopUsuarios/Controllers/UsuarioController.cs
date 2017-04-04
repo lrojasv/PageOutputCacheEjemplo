30c150d1b6d6150f715046346199908839559471
lrojasv <lrojasv@hotmail.es>
2017-04-04 10:21:29 -0500

    inicial

---
+++ WorkshopUsuarios/Controllers/UsuarioController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using WorkshopUsuarios.Models;
+
+namespace WorkshopUsuarios.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private static List<Usuario> RepositorioUsuario { get; set; }
+
+        [OutputCache(CacheProfile = "OneMinuteValidate")]
+        public ActionResult Index()
+        {
+            if(RepositorioUsuario==null)
+                LlenarRepositorioUsuario();
+            
+            return View(RepositorioUsuario);
+        }
+
+        public ViewResult Grabar()
+        {
+            return View(new Usuario());
+        }
+
+        public ViewResult Actualizar(int id)
+        {
+            var usuario = RepositorioUsuario.First(x => x.Id == id);
+            return View("Grabar", usuario);
+        }
+
+        public ViewResult GrabarUsuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                if (usuario.Id > 0)
+                {
+                    var usu = RepositorioUsuario.First(x => x.Id == usuario.Id);
+                    usu.Nombre = usuario.Nombre;
+                    usu.ApellidoPaterno = usuario.ApellidoPaterno;
+                    usu.ApellidoMaterno = usuario.ApellidoMaterno;
+                    usu.Edad = usuario.Edad;
+                    usu.Email = usuario.Email;
+                    usu.Login = usuario.Login;
+                    usu.Password = usuario.Password;
+                    return View("Index",RepositorioUsuario);
+                }
+
+                usuario.Id = ObtenerCorrelativoUsuario();
+                RepositorioUsuario.Add(usuario);
+                ViewBag.MensajeSatisfactorio = "El usuario se registro satisfactoriamente.";
+                ModelState.Clear();
+            }
+
+            return View("Grabar");
+        }
+
+        private int ObtenerCorrelativoUsuario()
+        {
+            return RepositorioUsuario.Max(x => x.Id + 1);
+        }
+
+        private void LlenarRepositorioUsuario()
+        {
+            RepositorioUsuario = new List<Usuario>
+            {
+                new Usuario
+                {
+                    Id = 1,
+                    Nombre = "Luis",
+                    ApellidoPaterno = "Rojas",
+                    ApellidoMaterno = "Vásquez",
+                    Edad = 29,
+                    Email = "lrojas@avances.com.pe",
+                    Login = "lrojas"
+                },
+                new Usuario
+                {
+                    Id = 2,
+                    Nombre = "Juan",
+                    ApellidoPaterno = "Perez",
+                    ApellidoMaterno = "Carranza",
+                    Edad = 30,
+                    Email = "jperez@avances.com.pe",
+                    Login = "jperez"
+                },
+                new Usuario
+                {
+                    Id = 3,
+                    Nombre = "Miguel",
+                    ApellidoPaterno = "Dávila",
+                    ApellidoMaterno = "Lazón",
+                    Edad = 26,
+                    Email = "mdavila@avances.com.pe",
+                    Login = "mdavila"
+                }
+            };
+        }
+	}
+}
---
+++ WorkshopUsuarios/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkshopUsuarios.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(20,ErrorMessage = "El nombre no debe exceder a los 20 caracteres.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido paterno es obligatorio.")]
+        [StringLength(20, ErrorMessage = "El apellido paterno no debe exceder a los 20 caracteres.")]
+        public string ApellidoPaterno { get; set; }
+
+        [Required(ErrorMessage = "El apellido materno es obligatorio.")]
+        [StringLength(20, ErrorMessage = "El apellido materno no debe exceder a los 20 caracteres.")]
+        public string ApellidoMaterno { get; set; }
+
+        [Required(ErrorMessage = "La edad es obligatoria.")]
+        [Range(1,120,ErrorMessage = "La edad no puede ser menor de 1 y mayor de 120.")]
+        public int Edad { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [RegularExpression("^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[a-z0-9]+)*\\.([a-z]{2,4})$",ErrorMessage = "El email ingresado es incorrecto.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El login es obligatorio.")]
+        [StringLength(15, ErrorMessage = "El login no debe exceder a los 15 caracteres.")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "El password es obligatorio.")]
+        [StringLength(15, ErrorMessage = "El nombre no debe exceder a los 15 caracteres.")]
+        public string Password { get; set; }
+
+    }
+}
